7a6c11ca1c569ba7ea7b2c604e6048e2cb95cf15
Alexey Cherkasov <alcherkasov@gmail.com>
2021-01-05 17:55:08 +0500

    Fix isPrime function

--- app/js/games/game-prime.js
+++ app/js/games/game-prime.js
@@ -1,7 +1,7 @@
 const isPrime = (num) => {
   if (num <= 1) return false;
 
-  for (let i = 2; i < num / 2; i += 1) {
+  for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) return false;
   }
 
